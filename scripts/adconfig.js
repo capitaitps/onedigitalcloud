9fbce763d4abc37ef6252f29d2523f49434eb56a
capitaitps <33121844+capitaitps@users.noreply.github.com>
2017-10-26 12:21:33 +0100

    Adde other files

---
+++ scripts/adconfig.js
@@ -0,0 +1,8 @@
+var appName = "One Digital Cloud";
+var appId = "369";
+var isPortalEnable=true;
+var isTranslationEnable=false;
+var isGoogleAdvertise=false;
+var isGoogleAnalytics=false;
+var buildEnvironment=1;
+var googleAdBanner="";
